9c189ddc571878193d0ebb8b4de9eb89c7acb371
Andrew <andrewthresh@yahoo.co.uk>
2021-12-14 11:00:25 -0500

    nice

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Text,
+  View,
+} from "react-native";
+import logo from "./assets/logo.png";
 
-export default function App() {
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState("1");
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.view}>
+      <Image source={logo} style={{ width: 505, height: 359 }} />;
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Type feedback here"
+        keyboardType="numeric"
+      />
+      <Text style={{ color: "#0", fontSize: 30 }}> Lets convert!!!</Text>
+      <Text style={{ color: "#999", fontSize: 60 }}>
+        {(text * 0.7).toFixed(2)}
+      </Text>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 0,
+    padding: 10,
+    backgroundColor: "#eaff47",
+  },
+  view: {
+    alignItems: "center",
+    backgroundColor: "#eaff47",
+    height: "100%",
   },
 });
+export default UselessTextInput;
